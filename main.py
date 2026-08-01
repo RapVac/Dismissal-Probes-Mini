8c1e31a155ffc52e39c6b28fc5b889767997a9ef
RapVac <89621646+RapVac@users.noreply.github.com>
2026-08-01 09:58:31 -0700

    Bug chasing

--- main.py
+++ main.py
@@ -17,43 +17,46 @@ def _load(name):
         file = pickle.load(f)
     return file
 
-## 1. Get labeled list of deception modes (accepted, rejected, n/a) + associated CoT.
-## (class, prompt)
-dataset = setup.load_dataset(project_home + training_data + activation_dataset)
+def main():
+    ## 1. Get labeled list of deception modes (accepted, rejected, n/a) + associated CoT.
+    ## (class, prompt)
+    dataset = setup.load_dataset(project_home + training_data + activation_dataset)
 
-## 2. Extract activations from pass thru target OW model.
-model, tokenizer = run_activations.init_model(model_id)
-hooks = run_activations.add_hooks(model, *layers)
+    ## 2. Extract activations from pass thru target OW model.
+    model, tokenizer = run_activations.init_model(model_id)
+    hooks = run_activations.add_hooks(model, *layers)
 
-probes = {}
+    probes = {}
 
-layer_probes = train_probes.train_probe(layers, dataset, model, tokenizer)
+    layer_probes = train_probes.train_probe(layers, dataset, model, tokenizer)
 
-for ((scaler, pca, probe), layer) in zip(layer_probes, layers):
-    probes[layer] = (scaler, pca, probe)
+    for ((scaler, pca, probe), layer) in zip(layer_probes, layers):
+        probes[layer] = (scaler, pca, probe)
 
-    _save(probe, f"probe_l{layer}.pkl")
-    _save(scaler, f"scaler_l{layer}.pkl")
-    _save(pca, f"pca_l{layer}.pkl")
+        _save(probe, f"probe_l{layer}.pkl")
+        _save(scaler, f"scaler_l{layer}.pkl")
+        _save(pca, f"pca_l{layer}.pkl")
 
-## 4. Run Against benchmarks.
-print(f"[+] Started benchmark...")
-benchmark1 = benchmark.DeceptionBench(model_id)
+    ## 4. Run Against benchmarks.
+    print(f"[+] Started benchmark...")
+    benchmark1 = benchmark.DeceptionBench(model_id)
 
-benchmark1.prepare_dataset()
-##deception_bench_output = benchmark1.run_all()
-##_save(deception_bench_output, "deception_bench_output.pkl")
-##benchmark1.kill_vllm()
-##
-##print(f"[+] Started analysis...")
-##for i, layer in enumerate(layers):
-##    print(f"[+] {time.asctime()} | Now on layer {layer}.")
-##    results = analyze.benchmark_output_to_probe_scores(deception_bench_output,
-##                                                       i,
-##                                                       layers,
-##                                                       model_id,
-##                                                       probes[layer][0],
-##                                                       probes[layer][1],
-##                                                       probes[layer][2])
-##    _save(results, f"results_l{layer}.pkl")
+    benchmark1.prepare_dataset()
+    deception_bench_output = benchmark1.run_all()
+    _save(deception_bench_output, "deception_bench_output.pkl")
+    benchmark1.kill_vllm()
+    
+    print(f"[+] Started analysis...")
+    for i, layer in enumerate(layers):
+        print(f"[+] {time.asctime()} | Now on layer {layer}.")
+        results = analyze.benchmark_output_to_probe_scores(deception_bench_output,
+                                                           i,
+                                                           layers,
+                                                           model_id,
+                                                           probes[layer][0],
+                                                           probes[layer][1],
+                                                           probes[layer][2])
+        _save(results, f"results_l{layer}.pkl")
 
+if __name__ == "__main__":
+    main()
